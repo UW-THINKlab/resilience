77129290756a6a580e21f6fcd8e54cd5b1822076
Landung 'Don' Setiawan <landungs@uw.edu>
2024-08-14 17:00:45 -0700

    feat: Add constants, catalogs, and i18n

---
+++ src/support_sphere/lib/utils/string_catalog.dart
@@ -0,0 +1,22 @@
+/// Texts used in the app
+class AppTexts {
+  static const String appName = 'Support Sphere';
+  static const String signUpWelcome = 'Welcome to ${AppTexts.appName}\nCreate a new account and prepare with your community';
+}
+
+/// Login related texts
+class LoginTexts {
+  static const String login = 'Login';
+  static const String loginIntoExisting = 'Login into an existing account';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String confirmPassword = 'Confirm Password';
+  static const String username = 'Username';
+  static const String signUpCode = 'Sign Up Code';
+  static const String forgotPassword = 'Forgot password?';
+  static const String dontHaveAnAccount = 'Don\'t have an account?';
+  static const String alreadyHaveAnAccount = 'Already have an account?';
+  static const String signUp = 'Sign Up';
+}
+
+
