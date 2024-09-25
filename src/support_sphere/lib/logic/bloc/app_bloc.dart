4e2059f8614625bd2f4ade0e15665c00774c9aac
Landung 'Don' Setiawan <landungs@uw.edu>
2024-09-25 09:17:07 -0700

    feat: Implement change in mode to app bloc

--- src/support_sphere/lib/logic/bloc/app_bloc.dart
+++ src/support_sphere/lib/logic/bloc/app_bloc.dart
@@ -1,21 +1,45 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:support_sphere/data/models/operational_event.dart';
+import 'package:support_sphere/data/repositories/app.dart';
 
 part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc() : super(const AppState()) {
+  AppBloc({required AppRepository appRepository})
+      : _appRepository = appRepository,
+        super(const AppState()) {
     on<AppOnBottomNavIndexChanged>(_onBottomNavIndexChanged);
     on<AppOnModeChanged>(_onModeChanged);
+    on<AppOnStatusChangeRequested>(_updateStatus);
+
+    _operationalStatusSubscription = _appRepository.operationalStatus.listen(
+      (data) => add(AppOnModeChanged(data.appMode)),
+    );
   }
+
+  final AppRepository _appRepository;
+  late final StreamSubscription<OperationalEvent>
+      _operationalStatusSubscription;
 
   void _onModeChanged(AppOnModeChanged event, Emitter<AppState> emit) {
     emit(state.copyWith(mode: event.mode));
   }
 
+  void _updateStatus(AppOnStatusChangeRequested event, Emitter<AppState> emit) {
+    _appRepository.changeOperationalStatus(operationalStatus: event.mode.name);
+  }
+
   void _onBottomNavIndexChanged(
       AppOnBottomNavIndexChanged event, Emitter<AppState> emit) {
     emit(state.copyWith(selectedBottomNavIndex: event.selectedBottomNavIndex));
+  }
+
+  @override
+  Future<void> close() {
+    _operationalStatusSubscription.cancel();
+    return super.close();
   }
 }
