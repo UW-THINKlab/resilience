9eeb6ca5bf7cbbad0686afad3dac2a6bc48dadac
Landung 'Don' Setiawan <landungs@uw.edu>
2024-09-25 09:07:19 -0700

    feat: Add app mode change events

--- src/support_sphere/lib/logic/bloc/app_event.dart
+++ src/support_sphere/lib/logic/bloc/app_event.dart
@@ -5,6 +5,24 @@ class AppEvent extends Equatable {
   List<Object> get props => [];
 }
 
+class AppOnModeChanged extends AppEvent {
+  AppOnModeChanged(this.mode);
+
+  final AppModes mode;
+
+  @override
+  List<Object> get props => [mode];
+}
+
+class AppOnStatusChangeRequested extends AppEvent {
+  AppOnStatusChangeRequested(this.mode);
+
+  final AppModes mode;
+
+  @override
+  List<Object> get props => [mode];
+}
+
 class AppOnBottomNavIndexChanged extends AppEvent {
   AppOnBottomNavIndexChanged(this.selectedBottomNavIndex);
 
