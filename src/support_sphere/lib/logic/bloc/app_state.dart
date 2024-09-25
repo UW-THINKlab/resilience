805b0ad38ba2a3aa05575405a08b7bc3a711fe88
Landung 'Don' Setiawan <landungs@uw.edu>
2024-09-25 11:11:55 -0700

    feat: Add 'currentUserRole' attribute for AppState

--- src/support_sphere/lib/logic/bloc/app_state.dart
+++ src/support_sphere/lib/logic/bloc/app_state.dart
@@ -8,21 +8,25 @@ enum AppModes {
 
 class AppState extends Equatable {
   const AppState({
+    this.currentUserRole = '',
     this.mode = AppModes.normal,
     this.selectedBottomNavIndex = 0,
   });
 
+  final String currentUserRole;
   final AppModes mode;
   final int selectedBottomNavIndex;
 
   @override
-  List<Object> get props => [mode, selectedBottomNavIndex];
+  List<Object> get props => [currentUserRole, mode, selectedBottomNavIndex];
 
   AppState copyWith({
+    String? currentUserRole,
     AppModes? mode,
     int? selectedBottomNavIndex,
   }) {
     return AppState(
+      currentUserRole: currentUserRole ?? this.currentUserRole,
       mode: mode ?? this.mode,
       selectedBottomNavIndex: selectedBottomNavIndex ?? this.selectedBottomNavIndex,
     );
