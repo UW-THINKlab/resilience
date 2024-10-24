c0fbf29f8c29ac5b759b6bb62588e800066d677d
Landung 'Don' Setiawan <landungs@uw.edu>
2024-10-24 11:44:51 -0700

    refactor: Remove non-existing widget for now

--- src/support_sphere/lib/presentation/pages/main_app/manage_resources/manage_resources_body.dart
+++ src/support_sphere/lib/presentation/pages/main_app/manage_resources/manage_resources_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:support_sphere/presentation/pages/main_app/manage_resources/new_resource_skill.dart';
 
 class ManageResourcesBody extends StatelessWidget {
   const ManageResourcesBody({super.key});
