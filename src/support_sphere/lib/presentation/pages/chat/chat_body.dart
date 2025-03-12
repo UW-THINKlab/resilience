f05ae41e9d783c45c72ce5c2d867ca7d05741043
Paul Philion <pphlilion@istreamplanet.com>
2025-03-12 09:01:47 -0700

    changed recommended by build tools

--- src/support_sphere/lib/presentation/pages/chat/chat_body.dart
+++ src/support_sphere/lib/presentation/pages/chat/chat_body.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:ionicons/ionicons.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:support_sphere/data/models/auth_user.dart';
-import 'package:support_sphere/data/models/clusters.dart';
-import 'package:support_sphere/data/models/households.dart';
-import 'package:support_sphere/data/models/person.dart';
 import 'package:support_sphere/logic/bloc/auth/authentication_bloc.dart';
 import 'package:support_sphere/logic/cubit/profile_cubit.dart';
-import 'package:support_sphere/presentation/components/profile_section.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:support_sphere/constants/string_catalog.dart';
 
 /// Chat Body Widget
