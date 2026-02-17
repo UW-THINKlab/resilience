d06271318a46d1f91cdc4071419369c9b090b5c1
Paul Philion <pphlilion@istreamplanet.com>
2026-02-17 11:35:35 -0800

    Working backup and restore scripts: pixi run db-backup, pixi run db-restore <backupfile>

--- scripts/backup-kube.sh
+++ scripts/backup-kube.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# FIXME: Name file based on neighborhood. from... ENV?
+# Currently, Neighborhood is hardcoded in code.
+neighborhood=Laurelhurst
+
 pod_name=$(kubectl get pods | grep supabase-supabase-db | cut -f1 -d' ')
-backup_file="resilience-backup-`date +%Y-%m-%d-%H%M`.sql.gz"
+backup_file="backup-$neighborhood-`date +%Y-%m-%d-%H%M`.sql.gz"
 
 # Assumes PGPASSWORD is already set in env, which is true for container instances
 
-# NOTE: admin user is needed for
+# NOTE: admin user is needed for dumpall
 user=supabase_admin
 
 echo Backing up $pod_name into $backup_file
-#kubectl exec -it $pod_name -- bash -c "PGPASSWORD=$DB_PASSWORD pg_dump --schema=public -U $user $db_name" > $backup_file
 kubectl exec -it $pod_name -- bash -c "PGUSER=$user pg_dumpall" | gzip > $backup_file
 
 echo done.
--- scripts/restore-kube.sh
+++ scripts/restore-kube.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-# Backup the resilience DB
-
-# load the env...
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-. $SCRIPT_DIR/../.env
+# Restore the resilience DB from backup
 
 pod_name=$(kubectl get pods | grep supabase-supabase-db | cut -f1 -d' ')
 backup_file=$1
 user=supabase_admin
 
+echo Restoring $pod_name from $backup_file
 
-gunzip -c $backup_file | kubectl exec -it $pod_name -- bash -c "PGPASSWORD=$DB_PASSWORD psql -U $user $DB_NAME"
+gunzip -c $backup_file | kubectl exec -it $pod_name -- bash -c "psql -U $user $PGDATABASE"
