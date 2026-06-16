a12f204bddbf8b723d5cae8f673c878f650c1378
Paul Philion <pphlilion@istreamplanet.com>
2026-06-16 10:47:13 -0700

    Updating pixi scripts and docs

--- scripts/db-load.sh
+++ scripts/db-load.sh
@@ -5,7 +5,22 @@
 project="resilience"
 container_name="supabase_db_$project"
 
-sql_file=$1
+# Source - https://stackoverflow.com/a/677212
+CLI="supabase"
+
+if ! command -v $CLI >/dev/null 2>&1
+then
+    echo "$CLI could not be found. See https://supabase.com/docs/guides/local-development/cli/getting-started"
+    exit 1
+fi
+
+# check script paramater for sql file name
+if [ -z ${1+x} ]; then
+  sql_file="data.sql.gz"
+else
+  sql_file=$1
+fi
+
 if [ ! -f "$sql_file" ]; then
     echo "Expected file not found: $sql_file"
     exit 1
@@ -13,6 +28,6 @@ fi
 
 export PGUSER=${PGUSER:-supabase_admin}
 
-#supabase db reset --local
+supabase db reset --local
 
 gunzip -c $sql_file | docker exec -i $container_name bash -c "psql -U $PGUSER postgres"
