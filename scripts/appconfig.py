14dae53ea39481a1b4f2fe67cc0dc2c198862d59
Paul Philion <pphlilion@istreamplanet.com>
2026-07-04 12:49:03 -0700

    updates, and testing with a clean env

--- scripts/appconfig.py
+++ scripts/appconfig.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import sys
 import pprint
 
 ## params
@@ -13,34 +14,69 @@ import pprint
 # Manage the application configuration
 # assumes supabase is installed
 
+_dart_template = """
+// WARNING - This is a generated file. Do not edit. Do not commit to source control.
+// Generate this file with:
+//
+//     pixi run -e supabase config <neighborhood.json>
 
-# get the publishable key
-env_cmd = "supabase status -o env"
-
-dart_code = """
 import 'package:latlong2/latlong.dart' show LatLng;
 
-// INTENTION: AppConfig _instance should be generated with a specific build script, from config.
-
-class AppConfig {
+class AppConfig {{
   static const String neighborhood = "{neighborhood}";
-  static const LatLng location = LatLng({location[0]}, {location[1]});
-  static const String supabaseUrl = "http://{supabaseUrl}";
+  static const LatLng location = LatLng({location[1]}, {location[0]});
+  static const String supabaseUrl = "{supabaseUrl}";
   static const String supabaseAnonKey = "{supabaseAnonKey}";
-}
+}}
 """
 
-# load expected config file
-file_name = "neighborhood.json";
-# TODO: cmd arg
 
-with open(file_name) as f:
+def emit_dart_code(config) -> str:
+  return _dart_template.format_map(config)
+
+
+def load_publishable_key() -> str:
+  ## FIXME
+  # Using the supabase command (or perhaps a grep of .env)
+  # the publishable key can be gotten from a running/configured system
+  # get the publishable key
+  #env_cmd = "supabase status -o env | grep PUBLISHABLE_KEY | cut -d '=' -f 2"
+  #supa_json = os.popen(env_cmd).read()
+  #print(supa_json)
+  #config['supabaseAnonKey'] = supa_json['PUBLISHABLE_KEY']
+  #return "jh34kj5h34kjh....fi834kreuhv7378g"
+  pass
+
+
+def main() -> int:
+  # parse args
+  parser = argparse.ArgumentParser()
+  parser.add_argument("neighborhood_file", default="neighborhood.json", help="Neighborhood metatdata JSON file.")
+  parser.add_argument("-o", "--output", default=None, help="Write output to a specific file")
+  parser.add_argument("--dart", action="store_true", help="Generate Dart output")
+  args = parser.parse_args()
+
+  with open(args.neighborhood_file) as f:
     config = json.load(f)
-    #print(config)
 
-    #supa_json = os.popen(env_cmd).read()
-    #print(supa_json)
+    if args.dart:
+      output = emit_dart_code(config)
+      print(f"type:{type(output)}, value:{output}")
+    else:
+      output = json.dumps(config, indent=4)
+      print(f"type:{type(output)}, value:{output}")
 
-    #config['supabaseAnonKey'] = supa_json['PUBLISHABLE_KEY']
+    if args.output:
+      #with open(args.output, 'w') as output:
+      #  f.write(str(output))
+      print(f"Generated dart output to {args.output}")
+    else:
+      print(output)
 
-    #pprint.pprint(config)
+    return 0
+
+  return 1 # shouldn't get here
+
+
+if __name__ == '__main__':
+    sys.exit(main())
