42e1cfc875553078b1e54e155315284a59698400
Paul Philion <pphlilion@istreamplanet.com>
2026-08-04 19:02:28 -0700

    Renaming build script names to seed-db

--- scripts/generate-seed-db.sh
+++ scripts/generate-seed-db.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# To dump a clean copy of resilience database use: pixi run -e backend fresh-db
+# To dump a clean copy of resilience database use: pixi run -e backend seed-db
 #
-# This uses "dump-fresh-db.sh" to dump the data needed to initialize a new system,
+# This uses "dump-seed-db.sh" to dump the data needed to initialize a new system,
 # with no user data.
 
 script_dir=$(dirname "$0")
-dump_script='dump-fresh-db.sh'
+dump_script='dump-seed-db.sh'
 
-dump_file="resilience-init.sql"
+dump_file="seed.sql.gz"
 
 # Kubernetes version - runs in DB pod
 pod_name=$(kubectl get pods | grep supabase-supabase-db | grep Running | cut -f1 -d' ')
@@ -20,4 +20,4 @@ echo Creating $dump_file from $pod_name
 kubectl cp $script_dir/$dump_script $pod_name:/tmp/$dump_script
 
 # Execute the script on the POD, gzip the results to the file
-kubectl exec $pod_name -- /tmp/$dump_script > $dump_file
+kubectl exec $pod_name -- /tmp/$dump_script | gzip > $dump_file
