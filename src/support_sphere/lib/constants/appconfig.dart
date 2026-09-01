3a4c9dfc1715017a2e5c35b5d360188ef79d18eb
Paul Philion <pphlilion@istreamplanet.com>
2026-09-01 13:23:02 -0700

    initial branch

---
+++ src/support_sphere/lib/constants/appconfig.dart
@@ -0,0 +1,14 @@
+
+// WARNING - This is a generated file. Do not edit. Do not commit to source control.
+// Generate this file with:
+//
+//     pixi run -e supabase config -f <neighborhood.json>
+
+import 'package:latlong2/latlong.dart' show LatLng;
+
+class AppConfig {
+  static const String neighborhood = "Laurelhurst";
+  static const LatLng location = LatLng(47.658, -122.277);
+  static const String supabaseUrl = "http://laurelhurst.supportsphere.acmerocket.com";
+  static const String supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzI2NTU2NDAwLAogICJleHAiOiAxODg0MzIyODAwCn0.mAQE_wTdAytVXk6DKkm2tPFXRGmNrMw72sldwdXZGEo";
+}
